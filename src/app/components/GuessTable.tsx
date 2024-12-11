e35fe14bce50334e502401a592cb52c8fa7d7787
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2024-12-11 16:39:56 -0300

    fix: fixing the size breaking with multiples guesses

--- src/app/components/GuessTable.tsx
+++ src/app/components/GuessTable.tsx
@@ -12,39 +12,53 @@ const GuessingTable: FC<GuessingTableProps> = ({ dailyMember, guesses }) => {
 
     
     return (
-        <table className="table-auto w-7xl border-collapse border border-gray-200 mx-auto mt-8">
-            <thead>
-            <tr className='sticky top-0 z-9 bg-gray-100'>
-                {FIELDS.map((field, index) => (
-                <th
-                    key={index}
-                    className="text-lg font-semibold text-gray-800 border border-gray-200 px-4 py-2"
-                >
-                    {field[0]}
-                </th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
-            {dailyMember &&
-                guesses.map((guess, rowIndex) => (
-                <tr key={rowIndex} className="border border-gray-200">
-                    {FIELDS.map((field, colIndex) => (
-                    <td
-                        key={colIndex}
-                        className={`text-lg text-gray-800 text-center border border-gray-200 px-4 py-2 ${handleGuessColor(
-                        guess[field[1]],
-                        dailyMember[field[1] as keyof typeof dailyMember]
-                        )}
-                        `}
+        <div className="w-7xl border border-gray-200 mx-auto mt-8"
+            style={{
+                scrollbarColor: '#4A4A4A #F3F4F6',
+                scrollbarWidth: 'thin',
+            }}
+        >
+            <table className="table-auto w-full border-collapse">
+                <thead>
+                <tr className="sticky top-0 z-9 bg-gray-100">
+                    {FIELDS.map((field, index) => (
+                    <th
+                        key={index}
+                        className="text-lg font-semibold text-gray-800 border border-gray-200 px-4 py-2"
                     >
-                        {formatGuess(guess[field[1]], dailyMember[field[1] as keyof typeof dailyMember], field[1])}
-                    </td>
+                        {field[0]}
+                    </th>
                     ))}
                 </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+            </table>
+            <div className="max-h-[16rem] overflow-y-auto">
+                <table className="table-auto w-full border-collapse">
+                <tbody>
+                    {dailyMember &&
+                    guesses.map((guess, rowIndex) => (
+                        <tr key={rowIndex} className="border border-gray-200">
+                        {FIELDS.map((field, colIndex) => (
+                            <td
+                            key={colIndex}
+                            className={`text-lg text-gray-800 text-center border border-gray-200 px-4 py-2 ${handleGuessColor(
+                                guess[field[1]],
+                                dailyMember[field[1] as keyof typeof dailyMember]
+                            )}`}
+                            >
+                            {formatGuess(
+                                guess[field[1]],
+                                dailyMember[field[1] as keyof typeof dailyMember],
+                                field[1]
+                            )}
+                            </td>
+                        ))}
+                        </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
